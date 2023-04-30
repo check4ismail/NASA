56043922a68c8a16c4ec2700019dba836eb75628
Ismail Elmaliki <check4elmaliki@live.com>
2023-04-30 10:54:05 -0400

    Initial Commit

---
+++ NASA/NASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAApp.swift
+//  NASA
+//
+//  Created by Ismail Elmaliki on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct NASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
